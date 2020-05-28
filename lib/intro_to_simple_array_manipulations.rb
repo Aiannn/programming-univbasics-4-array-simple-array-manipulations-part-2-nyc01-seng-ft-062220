9b5b79f64a2d8a5db24e1db935c0bcadc5568817
Aian Soktoev <soktoevayan@gmail.com>
2020-05-28 00:40:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,7 @@ def using_concat(array_one, array_two)
 end
 
 def using_insert(array, name)
-  array.insert(3, name)
+  array.insert(4, name)
 end
 
 def using_uniq(array)
@@ -11,7 +11,7 @@ def using_uniq(array)
 end 
 
 def using_flatten(array)
-  array.flatten
+  array.flatten()
 end
 
 def using_delete(array, string)
